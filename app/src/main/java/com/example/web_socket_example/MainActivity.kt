6dc5fb39bc071e46a0cce7d9e30efc8b84c9d9d6
Bikash Das <bikash.das@portonics.com>
2021-08-10 05:24:17 +0600

    V1 completed.

--- app/src/main/java/com/example/web_socket_example/MainActivity.kt
+++ app/src/main/java/com/example/web_socket_example/MainActivity.kt
@@ -1,14 +1,24 @@
 package com.example.web_socket_example
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import kotlinx.android.synthetic.main.activity_main.*
 import okhttp3.*
 import okio.ByteString
+import java.text.SimpleDateFormat
 import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
+
+
+    private val displayDateStringFormat = "EEE, d MMM yyyy HH:mm:ss.SSS Z"
+    private val dateFormatter by lazy {
+        SimpleDateFormat(displayDateStringFormat, Locale.getDefault())
+    }
 
     private lateinit var client: OkHttpClient
     private lateinit var ws: WebSocket
@@ -19,59 +29,111 @@ class MainActivity : AppCompatActivity() {
         client = OkHttpClient()
 
         btnStart.setOnClickListener { start() }
-        btnSend.setOnClickListener { Date().toString().let {
-            println("Sending: ${it}")
-            ws.send(it)
-        } }
 
         btnStop.setOnClickListener {
             ws.close(EchoWebSocketListener.NORMAL_CLOSURE_STATUS, "Goodbye !")
         }
+
+        ivSendMessage.setOnClickListener {
+            etMessage.text?.toString()?.let {
+                if (it.isNotBlank()){
+                    ws.send(it)
+                    displaySend(it)
+                }
+            }
+            etMessage.setText("")
+        }
+
     }
 
     private fun start() {
         val request: Request = Request.Builder().url("ws://echo.websocket.org").build()
         println(request.url().toString())
-        val listener = EchoWebSocketListener()
+        val listener = EchoWebSocketListener(
+            ::displaySend,
+            ::displayReceived,
+            ::displayError,
+            ::displayStatusMessage
+        )
         ws = client.newWebSocket(request, listener)
 
     }
 
-    private class EchoWebSocketListener : WebSocketListener() {
+    private fun displaySend(message: String) {
+        runOnMainThread {
+            tvSendMessage.text = "${getTimeStamp()} | Sent : $message"
+            tvReceivedMessage.text = ""
+            tvErrorMessage.text = ""
+        }
+    }
+
+    private fun getTimeStamp() = dateFormatter.format(Date())
+
+    private fun displayReceived(message: String) {
+        runOnMainThread{
+            tvReceivedMessage.text = "${getTimeStamp()} | Received: $message"
+            tvErrorMessage.text = ""
+        }
+    }
+
+    private fun displayError(message: String) {
+        runOnMainThread {
+            tvReceivedMessage.text = ""
+            tvErrorMessage.text = "${getTimeStamp()} | Error: $message"
+        }
+    }
+
+    private fun displayStatusMessage(message: String) {
+        runOnMainThread {
+            tvStatus.text = "${getTimeStamp()} | Status: $message"
+        }
+    }
+
+    private class EchoWebSocketListener(
+        val displaySend: (String) -> Unit,
+        val displayReceived: (String) -> Unit,
+        val displayError: (String) -> Unit,
+        val displayStatusMessage: (String) -> Unit,
+    ) : WebSocketListener() {
+
         override fun onOpen(webSocket: WebSocket, response: Response?) {
-            println("Sending: Hello, it's SSaurel !")
-            webSocket.send("Hello, it's SSaurel !")
-//            webSocket.send("What's up ?")
-//            println("Sending:What's up ?")
-//            webSocket.send(ByteString.decodeHex("deadbeef"))
-//            println("Sending:${ByteString.decodeHex("deadbeef")}")
-//            webSocket.close(NORMAL_CLOSURE_STATUS, "Goodbye !")
+//            println("Sending: $initString")
+            displayStatusMessage("Connection opened......")
+//            webSocket.send(initString)
+//            displaySend(initString)
         }
 
         override fun onMessage(webSocket: WebSocket, text: String) {
-            println("Receiving : $text")
-
+            println("Received string : $text")
+            displayStatusMessage("Received string : $text")
+            displayReceived(text)
         }
 
         override fun onMessage(webSocket: WebSocket, bytes: ByteString) {
-            println("Receiving bytes : " + bytes.hex())
+            println("Received bytes : " + bytes.hex())
         }
 
         override fun onClosing(webSocket: WebSocket, code: Int, reason: String) {
             webSocket.close(NORMAL_CLOSURE_STATUS, null)
             println("Closing : $code / $reason")
+            displayStatusMessage("Connection closed : Code:$code, Reason: $reason")
         }
 
         override fun onFailure(webSocket: WebSocket?, t: Throwable, response: Response?) {
             println("Error : " + t.message)
+            displayStatusMessage("Error : " + t.message)
+            displayError("Error : " + t.message)
         }
 
         companion object {
             const val NORMAL_CLOSURE_STATUS = 1000
+            private const val initString = "Hello!"
         }
     }
+}
 
-    private fun output(message:String){
-
-    }
+fun <T> runOnMainThread(delayMs: Long = 0L, task: () -> T) {
+    Handler(Looper.getMainLooper()).postDelayed({
+        try{ task.invoke() }catch (ex:Throwable){ex.printStackTrace()}
+    }, delayMs)
 }
